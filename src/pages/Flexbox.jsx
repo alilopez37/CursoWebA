d27e8b638a78a069cd2e8ca2d01646f37c4c2494
alilopez <ali.lopez37@gmail.com>
2023-01-17 13:13:50 -0600

    [UPDATE] Add CSS

--- src/pages/Flexbox.jsx
+++ src/pages/Flexbox.jsx
@@ -1,7 +1,7 @@
 import FlexContainer from "../components/organism/FlexContainer";
 
 function Flexbox() {
-  const itemFlex = [1, 2, 3, 4, 5];
+  const itemFlex = [1, 2, 3, 4, 5,6];
   return (
     <>
       <FlexContainer itemFlex={itemFlex} />
