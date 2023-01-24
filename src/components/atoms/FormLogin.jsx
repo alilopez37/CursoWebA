d1d55e53024ca6e20b55d1f94ef79ef85dc578c4
alilopez <ali.lopez37@gmail.com>
2023-01-24 13:16:55 -0600

    [UPDATE] Cambios en FormLogin - event handler

--- src/components/atoms/FormLogin.jsx
+++ src/components/atoms/FormLogin.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/react.svg";
 
 function FormLogin() {
+  const [username, setUsername] = useState('')
+  const [error, setError] = useState('')
+
+  const handlerFocus = () => {
+    console.log("focus");
+  };
+
+  const handlerChange = (e) => {
+    setUsername(e.target.value)
+  };
+
+  const handlerClick = (e) => {
+    e.preventDefault();
+    setError('Error en la validación del nombre')
+  }
+
   return (
     <div>
       <img src={Logo} alt="Logo de la empresa" />
       <form>
-        <input type="text" />
+        <input type="text" value={username}
+        onFocus={handlerFocus} 
+        onChange={handlerChange}  />
         <input type="password" />
-        <button>Iniciar sesión</button>
+        <button onClick={handlerClick}>Iniciar sesión</button>
+        <label>{error}</label>
         <Link to="/register">Regístrate</Link>
       </form>
     </div>
